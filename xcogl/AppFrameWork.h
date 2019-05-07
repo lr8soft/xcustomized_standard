6da086cea4e55f341f564ddd08b2f533102770a8
LT_lrsoft <lrsoft@hotmail.com>
2019-05-07 22:30:52 +0800

    v0.7

--- xcogl/AppFrameWork.h
+++ xcogl/AppFrameWork.h
@@ -2,23 +2,25 @@
 #ifndef _ogl_class_
 #define _ogl_class_
 #include <GLFW/glfw3.h>
+#include "XCShape/XCRectangle.h"
 namespace xc_ogl {
 	class AppFrameWork
 	{
 	private:
+		xc_shape::XCRectangle *rect;
 		static AppFrameWork* app_ptr;
 		const char* title;
 		int width, height;
 		bool have_init = false;
-		GLuint vao, vbo,tbo;
-		GLuint main_program;
+		GLuint vao,vao_bg ,vbo,vbo_bg,tbo,tbo_bg;
+		GLuint program,program_bg;
 		GLFWwindow* screen;
 		static void screen_resize(GLFWwindow*,int,int);
 		virtual void display();
 		virtual void key_check();
 		virtual void render();
 		virtual void shader_init();
-		
+		enum{ VAOBG,VAOShape };
 	public:
 		AppFrameWork();
 		AppFrameWork(int width,int height,const char* title);
